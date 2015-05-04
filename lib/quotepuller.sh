ee0bcfc19c0938349997e177731036eee61fe43a
Marshall Farrier <info@marshallfarrier.com>
2015-05-03 20:44:45 -0700

    Pass parameters to shell script.

--- lib/quotepuller.sh
+++ lib/quotepuller.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-opts="$1"
+user=$(id -un)
+group=$(id -gn)
+TEMP=$(getopt -n "$0" -o d:l:s:v: --long dbconn:,logs:,serv:,venvs: -- "$@")
+if [ $? != 0 ] ; then echo "Terminating..." >&2 ; exit 1 ; fi
+eval set -- "$TEMP"
+while true;
+do
+    case "$1" in
+        -d|--dbconn)
+            dbconn="$2"
+            shift 2;;
+        -l|--logs)
+            logs="$2"
+            shift 2;;
+        -s|--serv)
+            service="$2"
+            shift 2;;
+        -v|--venvs)
+            venvs="$2"
+            shift 2;;
+        --)
+            shift
+            break;;
+    esac
+done
 
-echo "${1}"
-echo "user:"
-id -un
-echo "group:"
-id -gn
+echo "user: ${user}"
+echo "group: ${group}"
+echo "venvs: ${venvs}"
+echo "logs: ${logs}"
+echo "dbconn: ${dbconn}"
+echo "service: ${service}"
